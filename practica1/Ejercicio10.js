b59ba0634876b5c03c44b9f476292e396c7b5769
karenlim1011 <limqkaren@gmail.com>
2026-03-30 06:02:26 +0200

    Update Ejercicio10.js

---
+++ practica1/Ejercicio10.js
@@ -0,0 +1,3 @@
+//Un callback es una funcion que se ejecuta despues, es decir que puede imprimir despues de un determinado 
+//tiempo teniendo dos opciones ser sincrono 
+//o asincronico y la promesa es una forma mas ordenada de manejar cosas que tardan tiempo y un poco mas eficiente 
